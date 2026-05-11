6a7edfefabd59089e5535c06b597b724b73037d3
Seçkin Tokcan <tokcanseckin@users.noreply.github.com>
2026-05-11 15:09:19 +0300

    Set player content width to 1080px

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1294,7 +1294,7 @@ function PlayerView({
         </div>
       </header>
 
-      <main className="flex-1 min-h-0 p-4 max-w-[1360px] mx-auto w-full flex flex-col gap-3">
+      <main className="flex-1 min-h-0 p-4 max-w-[1080px] mx-auto w-full flex flex-col gap-3">
 
         {/* Controls + YouTube row */}
         <div
